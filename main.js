e35bc82aa54c1c90a9a2d8ba8ccc9d539685a9c6
ColeyG <cjgeerts@gmail.com>
2018-12-09 21:13:37 -0500

    socket init

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -53,5 +55,12 @@ app.on('activate', function () {
   }
 })
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
