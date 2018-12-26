b7c409646e47a4cd09bdac6298d81a287eb1cd28
ColeyG <cjgeerts@gmail.com>
2018-12-26 13:54:34 -0500

    clean up & examples

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var http = require('http');
-var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
 server.listen(3000);
+
 console.log('Server is listening on port 3000...');
+
 io.on('connection', function (socket) {
 
-    socket.on('my other event', function (data) {
+    socket.on('pinger', function (data) {
       console.log(data);
     });
 });
