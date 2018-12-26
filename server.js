a6e8c6cc16009f345f3c30ad690b571542c510df
ColeyG <cjgeerts@gmail.com>
2018-12-26 13:52:11 -0500

    working socket server

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var io = require('socket.io').listen(3000);
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(3000);
 console.log('Server is listening on port 3000...');
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
+
     socket.on('my other event', function (data) {
       console.log(data);
     });
