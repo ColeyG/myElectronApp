6be2d9daaa55f82c46ca075976944d9d8c94958b
ColeyG <cjgeerts@gmail.com>
2018-12-26 21:32:15 -0500

    adding logging ex

--- js/windowEvents.js
+++ js/windowEvents.js
@@ -1,10 +1,25 @@
 const remote = require('electron').remote;
-
-let close=document.querySelector('.close');
+const socket = io.connect('http://localhost:3000/');
 
 function closeHandler(){
     //all close events here. 
     window.close();
 }
 
-close.addEventListener('click',closeHandler,false);
+function submitMessage(e){
+    e.preventDefault();
+    let message=document.querySelector('.message-field').value;
+    //console.log(message);
+    socket.emit('send message',message);
+}
+
+socket.on('message recieve',(data)=>{
+    //console.log(data);
+    document.querySelector('.message-container').innerHTML+=data+"<br>";
+});
+
+document.querySelector('.close').addEventListener('click',closeHandler,false);
+document.querySelector('.submit-message').addEventListener('click',submitMessage,false);
+
+//example socket message:
+//socket.emit('pinger','asdf zxcv qwer');
--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const io = require('socket.io').listen(server);
 server.listen(3000);
 
 console.log('Server is listening on port 3000...');
+connections=[];
+log=[];
 
 io.on('connection', function (socket) {
+  connections.push(socket);
+  console.log('new connection, total connections: '+connections.length);
+    
+  socket.on('disconnect',function(data){
+    connections.splice(connections.indexOf(socket),1);
+    console.log('Disconected: %s sockects connected',connections.length);
+  });
 
-    socket.on('pinger', function (data) {
-      console.log(data);
-    });
+  socket.on('send message',(data)=>{
+    console.log('mes: '+data);
+    socket.emit('message recieve',data);
+  });
+
+  socket.on('pinger', function (data) {
+    console.log(data);
+  });
 });
